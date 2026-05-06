100546158d87baefdcf9a9cfa0b3ed44b8003682
Akshay_jayan <akshayjayan321@gmail.com>
2026-05-06 20:40:32 +0530

    corrections

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import CTASection from "../components/sections/CTASection";
 import StickyCTA from "../components/sections/StickyCTA";
 
 export default function Home() {
-  console.log(data);
   return <main>
     <Hero data={{ ...data.hero, facebookUrl: data.site.facebook }} phone={data.site.whatsapp} theme={data.theme} />
     {/* <ProblemGrid problems={data.problems} phone={data.site.whatsapp} theme={data.theme} /> */}
